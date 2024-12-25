398892d8d99f0eacbf603ac5b5da7593b0a66181
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-12-26 00:38:43 +0100

    //

--- app/(root)/(tabs)/request.tsx
+++ app/(root)/(tabs)/request.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import RideLayout from "@/components/RideLayout";
 
 const request = () => {
   return (
+    // <View style={styles.headerContainer}>
+    //   <GoogleTextInput />
+    // </View>
     <View>
-      <Text>request</Text>
+      <RideLayout title="">
+        <View></View>
+      </RideLayout>
     </View>
   );
 };
 
 export default request;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+    marginTop: 35,
+  },
+});
